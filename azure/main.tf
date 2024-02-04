8f985fa7393028bbe06c856eda33556148ea0873
cangelosilima <cangelosilima@gmail.com>
2024-02-04 20:48:32 -0300

    feat: add remote state

--- azure/main.tf
+++ azure/main.tf
@@ -1,9 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.90.0"
     }
+  }
+  backend "azurerm" {
+    key = "terraform-k8s.tfstate"
   }
 }
 
@@ -12,8 +15,27 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-    name = "fiap-tech-challenge"  
-    location = "eastus"
+  name       = "fiap-tech-challenge-k8s-group"
+  location   = "eastus"
+  managed_by = "fiap-tech-challenge-group"
+}
+
+resource "azurerm_storage_account" "storage_account_terraform" {
+  name                     = "tf-k8s-storage-account"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_storage_container" "storage_containe_terraform" {
+  name                  = "tf-k8s-storage-container"
+  storage_account_name  = azurerm_storage_account.storage_account_terraform.name
+  container_access_type = "private"
 }
 
 resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
@@ -43,7 +65,6 @@ output "client_certificate" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config_raw
-
+  value     = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config_raw
   sensitive = true
 }
