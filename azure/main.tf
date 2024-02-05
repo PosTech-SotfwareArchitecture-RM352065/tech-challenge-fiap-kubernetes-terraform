105b46ab368c23e446199f7a7f800fc5a613a84b
cangelosilima <cangelosilima@gmail.com>
2024-02-04 21:32:09 -0300

    fix: remove wrong resouce creation

--- azure/main.tf
+++ azure/main.tf
@@ -18,24 +18,10 @@ resource "azurerm_resource_group" "resource_group" {
   name       = "fiap-tech-challenge-k8s-group"
   location   = "eastus"
   managed_by = "fiap-tech-challenge-group"
-}
-
-resource "azurerm_storage_account" "storage_account_terraform" {
-  name                     = "tf-k8s-storage-account"
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 
   tags = {
     environment = "development"
   }
-}
-
-resource "azurerm_storage_container" "storage_containe_terraform" {
-  name                  = "tf-k8s-storage-container"
-  storage_account_name  = azurerm_storage_account.storage_account_terraform.name
-  container_access_type = "private"
 }
 
 resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
@@ -55,7 +41,7 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   }
 
   tags = {
-    Environment = "development"
+    environment = azurerm_resource_group.resource_group.tags["environment"]
   }
 }
 
